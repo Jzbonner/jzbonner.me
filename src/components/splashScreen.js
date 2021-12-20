73fcdfd246e59732b939aa6f0d8242798115bfd2
Jzbonner <jarrett.bonner@gmail.com>
2021-12-20 03:07:29 -0500

    (content-change) custom react hook for initial decal animation

--- src/components/splashScreen.js
+++ src/components/splashScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useContext } from "react"
 import styled from "styled-components"
 import Helmet from "react-helmet"
-import { motion, useAnimation } from "framer-motion"
+import { motion, MotionContext, useAnimation } from "framer-motion"
 
 import { lightTheme, darkTheme } from "../styles/theme"
 import Context from "../context/"
@@ -26,6 +26,7 @@ const StyledSplashScreen = styled(motion.div)`
     align-items: center;
     width: 110%; // adjust to fit logo size, hides logo on intial splash screen load
     height: 6rem;
+    // border: 1px solid white;
   }
   .backdrop {
     position: absolute;
@@ -37,6 +38,34 @@ const StyledSplashScreen = styled(motion.div)`
     background-color: ${({ theme, darkMode }) =>
       darkMode ? theme.colors.background : theme.colors.primary};
   }
+  .floating-decal {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    border: 1px solid white;
+    height: 100%;
+    width: 100%;
+  }
+  .initials-j {
+    float: left;
+    // position: fixed;
+    // bottom: 0;
+    // right: 0;
+    padding: 1rem;
+    // border: 1px solid red;
+    height: 5rem;
+  }
+  .initials-b {
+    float: left;
+    // position: fixed;
+    // bottom: 50;
+    // right: 50;
+    margin-left: -2.7rem;
+    padding: 1rem;
+    // border: 1px solid red;
+    height: 4.5rem;
+  }
 `
 
 const SplashScreen = () => {
@@ -44,12 +73,14 @@ const SplashScreen = () => {
 
   const backgroundControls = useAnimation()
   const backdropControls = useAnimation()
+  const decalControls = useAnimation()
 
   useEffect(() => {
     const sequence = async () => {
       await backgroundControls.start({ opacity: 0.8 })
       await backdropControls.start({ height: "0%", transition: { delay: 0.7 } })
-      await backgroundControls.start({ opacity: 0, transition: { delay: 0.4 } })
+      await decalControls.start({ opacity: 0.9, scale: 1 })
+      await backgroundControls.start({ opacity: 0, transition: { delay: 30 } })
       setState({ ...state, isIntroDone: true })
     }
     sequence()
@@ -65,6 +96,20 @@ const SplashScreen = () => {
       <Helmet
         bodyAttributes={{ class: !state.isIntroDone ? "splashScreen" : "" }}
       />
+      <motion.div className="floating-decal">
+        <img
+          className="initials-j"
+          src="https://res.cloudinary.com/dzmc7doja/image/upload/v1639985819/design-assets/design-icon-assets/letter-j.png"
+          initial={{ opacity: 0, scale: 0 }}
+          animate={decalControls}
+        />
+        <img
+          className="initials-b"
+          src="https://res.cloudinary.com/dzmc7doja/image/upload/v1639985819/design-assets/design-icon-assets/letter-b.png"
+          initial={{ opacity: 0, scale: 0 }}
+          animate={decalControls}
+        />
+      </motion.div>
       <div className="logo-wrapper">
         <motion.div
           className="backdrop"
